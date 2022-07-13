d1b410a5934117ddf976c95096109ab03216821f
Mitchellakr <103033728+Mitchellakr@users.noreply.github.com>
2022-07-13 20:08:19 +0100

    added more animation to the landing page

--- main.js
+++ main.js
@@ -5,15 +5,30 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
         splash.classList.add('display-none');
 
-    }, 1000);
+    }, 5000);
 });
-
-// var rule = CSSRulePlugin.getRule(".hero-btn"); //get the rule
-// gsap.to(rule, {duration: 5, cssRule: {backgroundColor: "#600", color: "white"}});
 
 gsap.from('.hero-content', { duration: 4, y: '100%', ease: 'back' })
 gsap.from('#heading', { duration: 2.2, y: '100%', ease: 'back', delay: .3 })
 gsap.from('#copy', { duration: 1.8, y: '100%', ease: 'back', delay: .4 })
 gsap.from('.hero-btn', { duration: 1.6, y: '100%', ease: 'back', delay: .5 })
 gsap.from('.first', { duration: 3, y: '80%', x: '200px', ease: 'back' })
-gsap.from('.third', { duration: 3, y: '80%', x: '-200px', ease: 'back'})
+gsap.from('.third', { duration: 3, y: '80%', x: '-200px', ease: 'back' })
+
+
+let header = document.querySelector(".header-sticky");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 60) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+// var rule = CSSRulePlugin.getRule(".hero-btn"); //get the rule
+// gsap.to(rule, {duration: 5, cssRule: {backgroundColor: "#600", color: "white"}});
+
+gsap.to('.nft2', { duration: 2, x: '250px', y: '-20px', ease: 'back', delay: 7, scale: 1.5 })
+gsap.to('.nft3', { duration: 2, x: '-80px', y: '40px', ease: 'back', delay: 7, scale: .7, z: -1 })
+gsap.to('.nft4', { duration: 2, x: '-250px', y: '-20px', ease: 'back', delay: 7, scale: 1.5 })
+gsap.to('.nft5', { duration: 2, x: '80px', y: '40px', ease: 'back', delay: 7, scale: .7 })
